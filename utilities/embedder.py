9b842f78fe60af9a2f7c7ce3f4b16056dc2612d3
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2024-08-12 20:03:24 -0600

    update embedder utils

--- utilities/embedder.py
+++ utilities/embedder.py
@@ -1,5 +1,16 @@
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import VectorStoreIndex
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+import os 
+
+embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+llm = OpenAI(model="gpt-4-turbo", api_key=os.getenv("OPENAI_API_KEY"))
+
+# Global Embed Model
+Settings.llm = llm
+Settings.embed_model = embed_model
+
 
 # TODO: Add 'typing' to func args
 def create_embedding(document): 
@@ -10,6 +21,7 @@ def create_embedding(document):
     return nodes
     
  # TODO: Test function logic   
-def init_vector_store(document): 
-    vector_store = VectorStoreIndex.from_documents(documents=document)
+def init_vector_store(nodes): 
+    vector_store = VectorStoreIndex(nodes)
+   
     return vector_store
